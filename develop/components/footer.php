90b6c25fa4a8cbf9080684d5f66dbfde4f47c5bd
BenitoLM <benito.lopez.tecno@gmail.com>
2025-01-21 01:10:46 -0600

    relojes

---
+++ develop/components/footer.php
@@ -0,0 +1,15 @@
+<footer class="mt-3" style="padding: 20px; border: 1px solid #ccc;">
+    <div class="flex justify-between align-center">
+        <div style="width: 230px;">
+            <img src="develop/img/logo-ig.png" alt="" />
+        </div>
+        <div class="flex justify-center align-center gap-3" style="font-size: 16px;">
+            <a href="">Agenda una cita</a>
+            <hr class="vertical">
+            <a href="">telefono</a>
+            <hr class="vertical">
+            <a href="">Mail</a>
+        </div>
+        <strong class="font-1" style="text-align: right;"> Edificio El Pantalón, Bosques <br />CDMX </strong>
+    </div>
+</footer>
